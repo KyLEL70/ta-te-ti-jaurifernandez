290631106ebf634eabd4145962d115366aabc595
Jauri Fernandez <jauri.fernandez@gmail.com>
2020-05-02 20:58:16 -0300

    Agrego función mostrarTitulo. close #2

--- ttt.c
+++ ttt.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+void mostrarTitulo();
+
 int main(int argc, char * argv[])
 {
-    printf("Super Ta-Te-Ti\n");
+    mostrarTitulo();
     return 0;
+}
+
+void mostrarTitulo()
+{
+    printf("Super Ta-Te-Ti\n");
+    return;
 }
